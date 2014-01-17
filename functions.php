5dfd222889ffa76e01cf59694c049aaf600d36da
svmatthews <matthews.sam@gmail.com>
2014-01-16 18:29:25 -0800

    fixing functions.php

--- functions.php
+++ functions.php
@@ -25,13 +25,99 @@
 
 	add_post_type_support('page', 'excerpt');
 
-	// instantiate foss4g theme customization register
-	function foss4g_theme_customizer( $wp_customize ) {
+	// instantiate aod theme customization register
+	function aod_theme_customizer( $wp_customize ) {
 		// remove default static_front_page register
 		$wp_customize->remove_section( 'static_front_page' );
 
 	// SITE TITLE & TAGLINE - no need for code since this is default :)
 
-	
-	add_action( 'customize_register', 'foss4g_theme_customizer', 11 );
+	// HERO
+		// hero text
+		$wp_customize->add_setting( 'aod_hero_text', array(
+			'default' 		=> 'Attention!',
+		) );
+		$wp_customize->add_control( 'aod_hero_text', array(
+			'label' 		=> 'Hero / Attention Text',
+			'section' 		=> 'aod_hero',
+			'priority' 		=> 1,
+		) );
+		// hero hyperlink text
+		$wp_customize->add_setting( 'aod_hero_link_text', array(
+			'default' 		=> 'Learn More',
+		) );
+		$wp_customize->add_control( 'aod_hero_link_text', array(
+			'label' 		=> 'Hero / Attention Hyperlink Text',
+			'section' 		=> 'aod_hero',
+			'priority' 		=> 2,
+		) );
+		// hero hyperlink
+		$wp_customize->add_setting( 'aod_hero_link', array(
+			'default' 		=> 'http:// ...',
+		) );
+		$wp_customize->add_control( 'aod_hero_link', array(
+			'label' 		=> 'Hero / Attention Hyperlink',
+			'section' 		=> 'aod_hero',
+			'priority' 		=> 3,
+		) );
+		// hero display button
+		$wp_customize->add_setting('aod_hero_display', array(
+	        'default' 		=> 0,
+		    ));
+	    $wp_customize->add_control('aod_hero_display', array(
+	        'settings' 		=> 'aod_hero_display',
+	        'label'    		=> 'Hide Hero',
+	        'section'  		=> 'aod_hero',
+	        'type'     		=> 'checkbox',
+	        'priority'		=> 4,
+	    ));
+		// description section
+		$wp_customize->add_section( 'aod_hero', array(
+	        'title' 		=> 'Hero',
+	        'description' 	=> 'Hero section',
+	        'priority' 		=> 100,
+	    ) );
+
+	// ABBREV. DESCRIPTION
+		$wp_customize->add_setting( 'aod_desc_text', array(
+			'default' 		=> 'aod description',
+		) );
+		$wp_customize->add_control( 'aod_desc_text', array(
+			'label' 		=> 'Description below title',
+			'section' 		=> 'aod_desc',
+			'priority' 		=> 1,
+		) );
+		// description section
+		$wp_customize->add_section( 'aod_desc', array(
+	        'title' 		=> 'Description',
+	        'description' 	=> 'Description section',
+	        'priority' 		=> 105,
+	    ) );
+
+	// LANDING PAGE BUTTON
+		// button hyperlink
+		$wp_customize->add_setting( 'aod_button_link', array(
+			'default' 		=> 'http:// ...',
+		) );
+		$wp_customize->add_control( 'aod_button_link', array(
+			'label' 		=> 'Button link',
+			'section' 		=> 'aod_button',
+			'priority' 		=> 4,
+		) );
+		// button text
+		$wp_customize->add_setting( 'aod_button_text', array(
+			'default' 		=> 'Learn more',
+		) );
+		$wp_customize->add_control( 'aod_button_text', array(
+			'label' 		=> 'Button text',
+			'section' 		=> 'aod_button',
+			'priority' 		=> 5,
+		) );
+		$wp_customize->add_section( 'aod_button', array(
+	        'title' 		=> 'Homepage Button',
+	        'description' 	=> 'Button on home page',
+	        'priority' 		=> 120,
+	    ) );
+	}
+	add_action( 'customize_register', 'aod_theme_customizer', 11 );
 ?>
